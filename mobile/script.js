8792720b0be570d7e8615ab6944438f66ee98c4f
danbender <mail@danbender.net>
2014-02-09 07:18:21 +0100

    put js in own file

---
+++ mobile/script.js
@@ -0,0 +1,20 @@
+var fullViewArticle = document.querySelector('.articles')
+var el = document.querySelector('.layer')
+
+fullViewArticle.addEventListener('click', function(ev) {
+  var className = "scaled-article-container"
+
+  if (el.classList) {
+    el.classList.toggle(className);
+  }else{
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0) {
+      classes.splice(existingIndex, 1);
+    }else{
+      classes.push(className);
+    }
+    el.className = classes.join(' ');
+  }
+}, true)
