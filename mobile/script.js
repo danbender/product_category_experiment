ca4e17cfd3520bf099aec389a8fce23d6e966316
danbender <mail@danbender.net>
2014-02-09 20:54:25 +0100

    hacky solution. slider works

--- mobile/script.js
+++ mobile/script.js
@@ -19,3 +19,36 @@ fullViewArticle.addEventListener('click', function(ev) {
     el.className = classes.join(' ');
   }
 }, true)
+
+
+//  a hacky version that works
+
+$(function() {
+
+      // var firstChild = $("#demo li:first-child").clone(),
+          // lastChild  = $("#demo li:last-child").clone(),
+          container  = $("#demo ul");
+
+      // firstChild.appendTo(container);
+      // lastChild.prependTo(container);
+
+      $("#demo").dragend({
+        jumpToPage: 2,
+        onSwipeEnd: function() {
+          var first = this.pages[0],
+              last = this.pages[this.pages.length - 1];
+
+          if (first === this.activeElement) {
+            this.jumpToPage(this.pages.length - 1 );
+          }
+
+          if (last === this.activeElement) {
+            this.jumpToPage(2);
+          }
+
+        },
+        afterInitialize: function() {
+          this.container.style.visibility = "visible";
+        }
+      });
+    });
