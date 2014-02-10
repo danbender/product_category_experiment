43324d3809931daa8f556dd8c96446caac7c26a6
danbender <mail@danbender.net>
2014-02-10 08:15:37 +0100

    commented out for the time being so experimental drag script works

--- mobile/script.js
+++ mobile/script.js
@@ -1,21 +1,21 @@
 // var fullViewArticle = document.querySelector('.articles')
-var fullViewArticle = document.querySelector('.articlesList')
-var el = document.querySelector('.layer')
+// var fullViewArticle = document.querySelector('.articlesList')
+// var el = document.querySelector('.layer')
 
-fullViewArticle.addEventListener('click', function(ev) {
-  var className = "scaled-article-container"
+// fullViewArticle.addEventListener('click', function(ev) {
+//   var className = "scaled-article-container"
 
-  if (el.classList) {
-    el.classList.toggle(className);
-  }else{
-    var classes = el.className.split(' ');
-    var existingIndex = classes.indexOf(className);
+//   if (el.classList) {
+//     el.classList.toggle(className);
+//   }else{
+//     var classes = el.className.split(' ');
+//     var existingIndex = classes.indexOf(className);
 
-    if (existingIndex >= 0) {
-      classes.splice(existingIndex, 1);
-    }else{
-      classes.push(className);
-    }
-    el.className = classes.join(' ');
-  }
-}, true)
+//     if (existingIndex >= 0) {
+//       classes.splice(existingIndex, 1);
+//     }else{
+//       classes.push(className);
+//     }
+//     el.className = classes.join(' ');
+//   }
+// }, true)
