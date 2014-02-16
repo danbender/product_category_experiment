986692427c3946bdbc294116dd3c602b7b9575f8
danbender <mail@danbender.net>
2014-02-16 19:53:58 +0100

    refactor

--- mobile/3/script.js
+++ mobile/3/script.js
@@ -1,57 +1,19 @@
-var slider = {
+$("#slider").on("scroll", function(e) {
+  $(".slide")
+  .css({
+    "background-position": $(e.target).scrollLeft()/12+ "% 0"
+  });
+});
 
-  el: {
-    slider: $("#slider"),
-    allSlides: $(".slide"),
-    sliderNav: $(".slider-nav"),
-    allNavButtons: $(".slider-nav a")
-  },
+$('nav').on('click', 'a', function(e) {
+  var width, position;
+  e.preventDefault();
+  position = $(this).data('id');
+  width = 300;
 
-  timing: 800,
-  slideWidth: 300,
+  $('#slider').animate({
+    scrollLeft: position * width
+  }, 800);
 
-  init: function() {
-    this.bindUIEvents();
-  },
-
-  bindUIEvents: function() {
-    // Either on manual scroll...
-    this.el.slider.on("scroll", function(event) {
-      slider.moveSlidePosition(event);
-    });
-    // or on click
-    this.el.sliderNav.on("click", "a", function(event) {
-      slider.handleNavClick(event, this);
-    });
-  },
-
-  moveSlidePosition: function(event) {
-    // the 6-100 are 'hard-coded'...they make it work for the specfic width and height
-    this.el.allSlides.css({
-      "background-position": $(event.target).scrollLeft()/6-100+ "px 0"
-    });
-  },
-
-  handleNavClick: function(event, el) {
-    event.preventDefault();
-    var position = $(el).attr("data-index");
-
-    this.el.slider.animate({
-      scrollLeft: position * this.slideWidth
-    }, this.timing);
-
-    this.changeActiveNav(el);
-  },
-
-  changeActiveNav: function(el) {
-    this.el.allNavButtons.removeClass("active");
-    $(el).addClass("active");
-  }
-
-};
-
-slider.init();
-
-// http://codepen.io/BaylorRae/pen/ImGBC
-// Originally added click links, so I ported over and re-wrote
-// slider based on http://css-tricks.com/slider-with-sliding-backgrounds/
+  return false;
+});
